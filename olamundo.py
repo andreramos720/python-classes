214155d0c1e01fd06f9e3766127eab135c20f546
andreramos720 <andreramos720@gmail.com>
2024-06-06 14:13:13 -0300

    Add files via upload

---
+++ olamundo.py
@@ -0,0 +1,6 @@
+Nome= "André Ramos dos Santos"
+Idade= 33 
+cidade= "Suzano-SP"
+Formacao= "estudando python"
+print(f"olá me chamo {Nome}, tenho {Idade} anos")
+print(f"Moro em {cidade} e atualmente estou estudando {Formacao}")
